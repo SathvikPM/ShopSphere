25d461a0343df8eb4e1139ddac199b107c8f63bf
Sathvik PM <SATHVIKPAAROL2002@GMAIL.COM>
2025-10-03 23:53:04 +0530

    created repositoy layer and service layer

---
+++ ShopSphere/src/main/java/com/ShopSphere/ShopSphere/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.ShopSphere.ShopSphere.repository;
+
+import com.ShopSphere.ShopSphere.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
---
+++ ShopSphere/src/main/java/com/ShopSphere/ShopSphere/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,4 @@
+package com.ShopSphere.ShopSphere.service.impl;
+
+public class CategoryServiceImpl {
+}
